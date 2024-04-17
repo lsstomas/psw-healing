e66516bb5d8c66b72a730df5f3ae708b7e899b4e
Luis <luisftomasprado@gmail.com>
2024-04-17 00:16:18 -0300

    feat: implement the signup view and improve the messages system

--- users/views.py
+++ users/views.py
@@ -1,5 +1,67 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-    return render(request, "signup.html")
+    if request.method == "GET":
+        return render(request, "signup.html")
+
+    elif request.method == "POST":
+        form_data = {
+            "username": request.POST.get("username"),
+            "email": request.POST.get("email"),
+            "password1": request.POST.get("password1"),
+            "password2": request.POST.get("password2")
+        }
+
+        if User.objects.filter(username=form_data["username"]).exists():
+            messages.add_message(
+                request,
+                constants.ERROR,
+                "O nome de usuário já existe. Por favor, tente novamente!"
+            )
+            return redirect("signup")
+
+        elif form_data['password1'] != form_data['password2']:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                "As senhas não correspondem. Por favor, tente novamente!"
+            )
+            return redirect("signup")
+
+        elif len(form_data['password1']) < 8:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                "A senha deve ter pelo menos 8 caracteres. Por favor, tente novamente!"
+            )
+            return redirect("signup")
+
+        else:
+            try:
+                user = User.objects.create_user(
+                    form_data["username"],
+                    form_data["email"],
+                    form_data["password1"]
+                )
+                user.save()
+
+                messages.add_message(
+                    request,
+                    constants.SUCCESS,
+                    "O usuário foi criado com sucesso!"
+                )
+
+                return redirect("login")
+
+            except Exception as e:
+                messages.add_message(
+                    request,
+                    constants.ERROR,
+                    f"Erro ao criar usuário: {str(e)}"
+                )
+
+                return redirect("signup")
